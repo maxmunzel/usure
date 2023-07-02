a233c3b07595c8595c04e30e78c7b53a99bcee3d
Max Nagy <maxmunzel@me.com>
2023-07-02 17:23:09 +0200

    Removed SyncState, added State.__repr__

--- src/tlapy/sync.py
+++ src/tlapy/sync.py
@@ -1,20 +1,15 @@
 from typing import NamedTuple
 import random
-from enum import Enum, auto
 
-
-class SyncState(int, Enum):
-    Idle = auto()
-    Waiting = auto()
+N = 3
 
 
 class State(NamedTuple):
     inflight_limit: int = 3
-    open_msgs: tuple[int] = (1, 2, 3)
+    open_msgs: tuple[int] = tuple(range(N))
     received_msgs: frozenset[int] = frozenset()
     http_ok: tuple[int] = tuple()
     http_err: tuple[int] = tuple()
-    sync: SyncState = SyncState.Idle
 
     def next(self):
         yield from self.send_message()
@@ -22,49 +17,50 @@ class State(NamedTuple):
         yield from self.handle_ok()
 
     def send_message(self):
-        if (
-            len(self.http_ok) + len(self.http_err) < self.inflight_limit
-            and self.open_msgs
-            and self.sync == SyncState.Idle
-        ):
+        if len(self.http_ok) + len(self.http_err) < self.inflight_limit and self.open_msgs:
             msg = (self.open_msgs[0],)
             # success
-            yield f"suc", self._replace(
-                http_ok=self.http_ok + msg, received_msgs=self.received_msgs | set(msg), sync=SyncState.Waiting
-            )
+            yield f"suc", self._replace(http_ok=self.http_ok + msg, received_msgs=self.received_msgs | set(msg))
             # failure
-            yield "fail", self._replace(http_err=self.http_ok + msg, sync=SyncState.Waiting)
+            yield "fail", self._replace(http_err=self.http_ok + msg)
         return
         yield
 
     def handle_err(self):
         for i, err in enumerate(self.http_err):
             new_http_err = tuple(msg for j, msg in enumerate(self.http_err) if i != j)
-            yield f"err {err}", self._replace(http_err=new_http_err, sync=SyncState.Idle)
+            yield f"err {err}", self._replace(http_err=new_http_err)
         return
         yield
 
     def handle_ok(self):
         for i, msg in enumerate(self.http_ok):
             new_http_ok = tuple(msg for j, msg in enumerate(self.http_ok) if i != j)
-            if not self.open_msgs:
+            if not self.open_msgs or self.open_msgs[0] != msg:
                 # ignore if nothing left to send
                 new_open_msgs = self.open_msgs
             else:
-                # otherwise pop one from queue
+                # otherwise pop one from queue, iff its the correct one
                 new_open_msgs = self.open_msgs[1:]
 
-            yield f"ok {msg}", self._replace(http_ok=new_http_ok, open_msgs=new_open_msgs, sync=SyncState.Idle)
+            yield f"ok {msg}", self._replace(http_ok=new_http_ok, open_msgs=new_open_msgs)
         return
         yield
 
     def safety(self):
         deadlock = len(list(self.next())) == 0
-        if deadlock and self.received_msgs != {1, 2, 3}:
+        if deadlock:
+            return False
+        if deadlock and self.received_msgs != set(range(N)):
             return False
         if deadlock and len(self.open_msgs) != 0:
             return False
         return True
+
+    def __repr__(self):
+        return (
+            f"<open: {self.open_msgs} ok: {self.http_ok} fail: {self.http_err} recv: {set(self.received_msgs) or '{}'}>"
+        )
 
 
 def check(s: State):
@@ -94,6 +90,7 @@ def check(s: State):
         return True
 
     expand(s)
+    return len(states)
 
 
 def trace(s: State, max_len=100):
@@ -107,5 +104,4 @@ def trace(s: State, max_len=100):
 
 
 if __name__ == "__main__":
-    # print(check(State()))
-    print(trace(State()))
+    print(check(State()))
